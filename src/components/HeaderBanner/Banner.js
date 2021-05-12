4f06a0883cc08728b2f0ad69d299fd8a8cf551a5
SanchaiPengboot <sanchaipangboot@gmail.com>
2021-05-12 12:37:57 +0700

    Commid Submenu

--- src/components/HeaderBanner/Banner.js
+++ src/components/HeaderBanner/Banner.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 import './TopBanner.css'
 
 
@@ -8,23 +7,27 @@ export default class TopBanner extends Component {
     render() {
         return (
             <div>
-                <div>
+
+                <div className="bannner">
                     <img src="https://th-shop.in.th/img/logo/lognav.png" alt="Logo" />
                 </div>
-                    
+
+                
+
                 <div className="dropdown">
 
-                <ul className="nav__menu">
-                    <li className="nav__menu-item">Home</li>
-                    <li className="nav__menu-item">About</li>
-                    <ul className="nav__submenu">
-                        <li className="nav__submenu-item ">Our Company</li>
-                        <li className="nav__submenu-item ">Our Team</li>
-                        <li className="nav__submenu-item ">Our Portfolio</li>
+                    <ul className="nav__menu">
+                        <li className="nav__menu-item">Home</li>
+                        <li className="nav__menu-item">About</li>
+                            <ul className="nav__submenu">
+                                <li className="nav__submenu-item ">Our Company</li>
+                                <li className="nav__submenu-item ">Our Team</li>
+                                <li className="nav__submenu-item ">Our Portfolio</li>
+                            </ul>
+                        <li className="nav__menu-item">Contact</li>
                     </ul>
-                    <li className="nav__menu-item">Contact</li>
-                </ul>
-                </div>
+                 </div>
+
             </div>
         )
     }
